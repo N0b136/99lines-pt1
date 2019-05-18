f8ed3b087435ad48a6f0829a053495e70bf07036
n0b136 <50467142+N0b136@users.noreply.github.com>
2019-05-17 22:04:41 -0500

    Tweaked code to fix a bug

--- app.js
+++ app.js
@@ -7,16 +7,18 @@ for (let friend of friends) {
 
     loop2:
     while (lineNum > 0) {
-        console.log(lineNum + ' lines of code in the file, ' + lineNum + ' lines of code; ' + friend + ' strikes one out, clears is all out, ' + lineNum + ' lines of code in the file');
-        --lineNum;
+        if (lineNum > 1) {
+            console.log(lineNum + ' lines of code in the file, ' + lineNum + ' lines of code; ' + friend + ' strikes one out, clears is all out, ' + lineNum + ' lines of code in the file');
+            --lineNum;
+        };
         if (lineNum == 1) {
             console.log(lineNum + ' line of code in the file, ' + lineNum + ' line of code; ' + friend + ' strikes one out, clears is all out, ' + lineNum + ' line of code in the file');
-            
-        }
+            --lineNum;
+        };
         if (lineNum == 0) {
             lineNum = 99
             break loop2;
-        }
-    }
+        };
+    };
 
 };
